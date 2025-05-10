3fa50b564e3723c7d86c0ac9d4f6bc28c751161b
muazahmadma <muazkhan098@gmail.com>
2025-05-10 15:52:29 +0500

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
-import logo from "../Assets/logo.png";
+import logo from "../Assets/MZ.png";
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { CgGitFork } from "react-icons/cg";
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://google.com"
+                href="https://medium.com/@muazkhan098"
                 target="_blank"
                 rel="noreferrer"
               >
